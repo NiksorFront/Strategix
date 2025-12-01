9bd338c37dfa4e0e6049f98661b6aa4defd67055
NiksorFront <Hukcop@ya.ru>
2025-12-01 12:56:04 +0300

    Вертска секции «Вызовы международного рынка»

--- frontend/src/widgets/market-response/ui/MarketResponse.vue
+++ frontend/src/widgets/market-response/ui/MarketResponse.vue
@@ -4,14 +4,76 @@
 
 <template>
   <section class="market-response">
-    <h1>Быстро отвечаем на вызовы</h1>
+    <h3 class="market-title">
+      Быстро отвечаем на вызовы международного рынка
+    </h3>
+    <p class="small-text market-description">
+      Профессиональная команда в&nbsp;области репутационного маркетинга. Мы&nbsp;создаём бренды, поддерживаем хорошую репутацию клиентов в&nbsp;интернете, прогнозируем возможные риски, кризисы и&nbsp;профессионально устраняем&nbsp;их.
+      <br>
+      <br>
+      Под разные ситуации находим решения, чтобы у&nbsp;вас была узнаваемость, доверие к&nbsp;вам, инвесторы потом к&nbsp;вам несли денег и&nbsp;доверяли. Мыслить нестандарнтно.
+    </p>
   </section>
 </template>
 
 <style scped>
 .market-response {
-    width: 100vw;
-    height: 20vh;
-    background-color: var(--background-light);
+    width: calc(100vw - 2 * var(--padding-section-x));
+    height: 26vh;
+
+    padding: 4vh var(--padding-section-x) 0;
+    background-color: var(--strategix-light);
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    @media(--tablet-width){
+      height: 22vh;
+
+      padding: 8vh var(--padding-section-x) 0;
+    }
+}
+
+.market-title{
+    width: 100%;
+    height: fit-content;
+
+    margin: 0;
+
+    font-family: "Liberty-MT", "Onest", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+    font-weight: 400;      
+    line-height: 120%;  
+    letter-spacing: 0;
+    text-align: left;
+    text-transform: none;
+
+    color: var(--background-dark);
+    font-size: clamp(25px, 2.166vw, 52px);
+
+    @media(--tablet-width){
+      width: 50.4%;
+      height: fit-content;
+    }
+
+    @media(--mobile-medium) {
+      font-size: min(14px, 2.917vh);
+    }
+    
+}
+
+.market-description{
+  width: 100%;
+  height: fit-content;
+
+  margin: 0.5vh 0 0;
+
+  color: var(--background-dark);
+  text-align: left;
+
+  @media(--tablet-width){
+    width: 49.6%;
+    height: fit-content;
+  }
 }
 </style>
--- frontend/src/widgets/our-team/ui/OurTeam.vue
+++ frontend/src/widgets/our-team/ui/OurTeam.vue
@@ -1,17 +1,24 @@
-<!-- <script setup lang="ts">
+<script setup lang="ts">
+import IndexSectionTitle from '@/shared/ui/index-section-title';
 
-</script> -->
+</script>
+
 
 <template>
   <section class="our-team">
-    <h1>Наша команда</h1>
+    <!-- <IndexSectionTitle>
+      Наша команда
+    </IndexSectionTitle> -->
   </section>
 </template>
 
-<style scped>
+<style scoped>
 .our-team {
-    width: 100vw;
-    height: 80vh;
-    background-color: var(--background-light);
+    width: calc(100vw - 2 * var(--padding-section-x));
+    height: 61vh;
+
+
+    padding: 0 var(--padding-section-x) 8vh;
+    background-color: var(--strategix-light);
 }
 </style>
